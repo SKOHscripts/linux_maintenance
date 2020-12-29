2f55e114d432aca33558803038227be789dce923
SKOH <corentin.michel@mailo.com>
2020-12-29 18:39:33 +0100

    trash-list modification

--- maintenance.sh
+++ maintenance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rouge='\e[1;31m'
-vert='\e[1;33m' 
-bleu='\e[1;34m' 
-violet='\e[1;35m' 
+vert='\e[1;33m'
+bleu='\e[1;34m'
+violet='\e[1;35m'
 vert='\e[1;32m'
 neutre='\e[0;m'
 
@@ -27,7 +27,7 @@ fi
 
 zenity --question --no-wrap --height 40 --width 300 --title  "Maintenance d'Ubuntu" --text "Lancer la maintenance complète ?"
 
-if [ $? == 0 ] 
+if [ $? == 0 ]
 then
     echo ""
     echo -e -n "$vert [1/7]$rouge MISE A JOUR DES PAQUETS "
@@ -40,7 +40,7 @@ then
     sudo apt update
     sudo apt full-upgrade -y
     echo " "
-    
+
     echo -e -n "$vert [2/7]$rouge MISE A JOUR DES SNAPS "
     for i in `seq 30 $COLUMNS`;
         do echo -n "."
@@ -49,8 +49,8 @@ then
     # notify-send -i system-software-update "Maintenance d'Ubuntu" "Mise à jour des snaps"
     snap list --all
     sudo snap refresh
-    echo " " 
-    
+    echo " "
+
     echo -e -n "$vert [3/7]$rouge AUTO-REMOVE "
         for i in `seq 20 $COLUMNS`;
         do echo -n "."
@@ -59,7 +59,7 @@ then
     # notify-send -i system-software-update "Maintenance d'Ubuntu" "Auto-remove"
     sudo apt autoremove --purge -y
     echo " "
-    
+
     echo -e -n "$vert [4/7]$rouge CLEAN "
         for i in `seq 14 $COLUMNS`;
         do echo -n "."
@@ -68,21 +68,13 @@ then
     # notify-send -i system-software-update "Maintenance d'Ubuntu" "Clean"
     sudo apt autoclean
     echo " "
-    
+
     echo -e -n "$vert [5/7]$rouge PURGE "
         for i in `seq 14 $COLUMNS`;
         do echo -n "."
     done
     echo -e " $neutre"
     # notify-send -i system-software-update "Maintenance d'Ubuntu" "Purge"
-
-    which localepurge > /dev/null
-    if [ $? = 1 ]
-    then
-	    sudo apt install -y localepurge
-    fi
-
-    sudo localepurge
 
     sudo apt purge $(COLUMNS=200 dpkg -l | grep "^rc" | tr -s ' ' | cut -d ' ' -f 2) -y
     echo " "
@@ -120,9 +112,9 @@ then
     done
 
     zenity --question --height 40 --width 300 --title "Maintenance d'Ubuntu" --text "Voulez-vous supprimer les fichiers de la corbeille antérieurs au <b>$TRASH</b> ?"
-    if [ $? == 0 ] 
+    if [ $? == 0 ]
 	then
-	    trash-empty 7
+	    trash-empty 21
 	fi
     notify-send -i dialog-ok "Maintenance d'Ubuntu" "Terminée avec succès"
 
